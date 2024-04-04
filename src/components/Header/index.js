9cd4985c1ca7704263cd29bb0457613ebdfa660c
YashGediyaExhibyte <yash.gediya@exhibytesolution.com>
2024-04-04 18:39:16 +0530

    WIP header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="px-7 py-4 bg-slate-400 flex justify-between items-center">
+      <div className="flex gap-10 items-center">
+        <div className="h-12 w-[150px] bg-slate-300 rounded-lg" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
